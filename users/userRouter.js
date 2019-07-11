c871ae041527655f988d2aca96fde88615ccae4b
Ang Xu <angxu1234@gmail.com>
2019-07-10 19:30:47 -0500

    MVP complete

--- users/userRouter.js
+++ users/userRouter.js
@@ -19,7 +19,18 @@ router.post("/", validateUser, async (req, res) => {
 	}
 });
 
-router.post("/:id/posts", (req, res) => {});
+router.post("/:id/posts", validateUserId, validatePost, async (req, res) => {
+	try {
+		const post = await postsDB.insert({ ...req.body, user_id: req.params.id });
+		res.status(201).json(post);
+	} catch (error) {
+		// log error to database
+		console.log(error);
+		res.status(500).json({
+			error: "There was an error while adding the user to the database"
+		});
+	}
+});
 
 router.get("/", async (req, res) => {
 	try {
@@ -36,7 +47,6 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", validateUserId, async (req, res) => {
 	try {
-		// const user = await usersDB.getById(req.params.id);
 		res.status(200).json(req.user);
 	} catch (error) {
 		// log error to database
@@ -47,11 +57,52 @@ router.get("/:id", validateUserId, async (req, res) => {
 	}
 });
 
-router.get("/:id/posts", async (req, res) => {});
+router.get("/:id/posts", validateUserId, async (req, res) => {
+	try {
+		const posts = await usersDB.getUserPosts(req.params.id);
+		if (posts.length === 0) {
+			res.status(201).json({
+				message: "The user has no posts."
+			});
+		} else {
+			res.status(200).json(posts);
+		}
+	} catch (error) {
+		// log error to database
+		console.log(error);
+		res.status(500).json({
+			error: "The user information could not be retrieved."
+		});
+	}
+});
 
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", validateUserId, async (req, res) => {
+	try {
+		const count = await usersDB.remove(req.params.id);
+		if (count > 0) {
+			res.status(200).json(req.user);
+		}
+	} catch (error) {
+		// log error to database
+		console.log(error);
+		res.status(500).json({
+			error: "The user could not be removed"
+		});
+	}
+});
 
-router.put("/:id", async (req, res) => {});
+router.put("/:id", validateUserId, validateUser, async (req, res) => {
+	try {
+		const user = await usersDB.update(req.params.id, req.body);
+		res.status(200).json(user);
+	} catch (error) {
+		// log error to database
+		console.log(error);
+		res.status(500).json({
+			error: "The user information could not be modified."
+		});
+	}
+});
 
 //custom middleware
 
@@ -79,23 +130,16 @@ function validateUser(req, res, next) {
 	if (!name)
 		return res.status(400).json({ message: "missing required name field" });
 	next();
-	// if (req.body) {
-	// 	if (req.body.name) {
-	// 		next();
-	// 	} else {
-	// 		res.status(400).json({ message: "missing required name field" });
-	// 	}
-	// } else {
-	// 	res.status(400).json({ message: "missing user data" });
-	// }
 }
 
 function validatePost(req, res, next) {
-	!!req.body
-		? !!req.body.text
-			? next()
-			: res.status(400).json({ message: "missing required text field" })
-		: res.status(400).json({ message: "missing post data" });
+	if (Object.keys(req.body).length === 0) {
+		res.status(400).json({ message: "missing post data" });
+	} else if (!req.body.text) {
+		res.status(400).json({ message: "missing required text field" });
+	} else {
+		next();
+	}
 }
 
 module.exports = router;
